43c11d8e05d1eed979479783bbc712826813d07b
Tristan Lanoye <tristanyjlanoye@gmail.com>
2020-02-02 12:21:11 +0100

    :bug: modify instance options

--- assets/scripts/CardBack.js
+++ assets/scripts/CardBack.js
@@ -46,3 +46,46 @@ export class CardBack extends Card {
         p.triangle(this.trianglePosition[0].x, this.trianglePosition[0].y, this.trianglePosition[1].x, this.trianglePosition[1].y, this.trianglePosition[2].x, this.trianglePosition[2].y)
     }
 }
+
+// Card Back 2
+export class CardBack2 extends Card {
+    constructor(p, opts) {
+        super(p, opts)
+
+        this.initTriangle()
+    }
+
+    initTriangle() {
+        this.triangleSize = {
+            width: 60,
+            height: 60
+        }
+        this.trianglePosition = [
+            {
+                x: this.innerCenter.x - this.triangleSize.width / 2,
+                y: this.innerCenter.y + this.triangleSize.height / 2
+            },
+            {
+                x: this.innerCenter.x,
+                y: this.innerCenter.y - this.triangleSize.height / 2
+            },
+            {
+                x: this.innerCenter.x + this.triangleSize.width / 2,
+                y: this.innerCenter.y + this.triangleSize.height / 2
+            }
+        ]
+    }
+
+    displayBack(p) {
+        p.noStroke()
+        p.fill(0)
+        p.rect(this.innerPosition.x, this.innerPosition.y, this.innerSize.width, this.innerSize.height)
+    }
+
+    displayShape(p) {
+        p.strokeWeight(6)
+        p.stroke(pick(this.palette))
+        p.fill(pick(this.palette))
+        p.triangle(this.trianglePosition[0].x, this.trianglePosition[0].y, this.trianglePosition[1].x, this.trianglePosition[1].y, this.trianglePosition[2].x, this.trianglePosition[2].y)
+    }
+}
